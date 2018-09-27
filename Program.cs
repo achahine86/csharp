057804ad58dc088c710a837d6ee74c8a5bb10137
Aline Ibrahin Chahine <35482820831@info.local>
2018-09-27 08:34:38 -0300

    c#

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Senai.Exemplo.Variaveis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Senai.Operadores.Exercicio7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Senai.Operadores.Exercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Informe o valor em US$");
+                decimal valorDolar = decimal.Parse(Console.ReadLine());
+
+                Console.WriteLine("Informe o valor em real");
+                decimal valorReal = decimal.Parse(Console.ReadLine());
+
+                decimal ConversaoDolarReal = valorDolar * valorReal;
+
+                // o cifrão $ significa interpolação
+                Console.WriteLine($"US$ {valorDolar} * R$ {valorReal} = {ConversaoDolarReal}");
+        }
+    }
+}
